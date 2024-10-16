173bdc173e1b989197ea2d5d93d27c4b7340790c
eswar1207 <116292896+eswar1207@users.noreply.github.com>
2024-10-16 10:59:48 +0530

    Update Register.js

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import api from '../api';
 import { useDispatch } from "react-redux";
 import { message, Form, Input, Button } from "antd";
 import "../styles/Register.css";
