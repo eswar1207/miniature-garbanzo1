71169188366396018f0159aa981187ed6b918204
eswar1207 <116292896+eswar1207@users.noreply.github.com>
2024-10-16 10:58:38 +0530

    Update CartPage.js

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -1,3 +1,4 @@
+import api from '../api';
 import React, { useState, useEffect } from "react";
 import DefaultLayout from "../components/DefaultLayout";
 import { useSelector, useDispatch } from "react-redux";
